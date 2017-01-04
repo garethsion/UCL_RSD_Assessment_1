03be9e92324eeacbc134ec8635f3ebc3137a549e
Gareth Jones <garethsion@googlemail.com>
2017-01-04 20:59:04 +0000

    Updated graph.py with some test functionality

--- greengraph/graph.py
+++ greengraph/graph.py
@@ -1,24 +1,54 @@
+#!/usr/bin/python
 import numpy as np
 import geopy
 from .map import Map
 
 class Greengraph(object):
     def __init__(self, start, end):
+        """ Constructor for Greengraph class
+        
+        Parameters
+        ----------
+        start: str
+            command line input argument for the source location
+        end: str
+            command line input argument for final location
+
+        """
         self.start=start
         self.end=end
         self.geocoder=geopy.geocoders.GoogleV3(domain="maps.google.co.uk")
         
     def geolocate(self, place):
+        """ """
         return self.geocoder.geocode(place, exactly_one=False)[0][1]
     
     def location_sequence(self, start,end,steps):
+        """ """
+
+        # Raise an error if the input argument types do not match what is expected
+        if type(start) != str:
+            raise TypeError("Source should be a *string* value")
+        if type(end) != str:
+            raise TypeError("Destination should be a *string* value")
+        
+        # Raise an error if steps is not a number 
+        if type(steps) != int & type(steps) != float:
+            raise TypeError("Step size must be an *integer* or *floating point* value") 
+        
+        # Raise an error if steps is negative
+        if steps < 0:
+            raise ValueError("Step size must be positive")
+
         lats = np.linspace(start[0], end[0], steps)
         longs = np.linspace(start[1],end[1], steps)
         return np.vstack([lats, longs]).transpose()
 
     def green_between(self, steps):
+        """ """
         return [Map(*location).count_green()
                 for location in self.location_sequence(
                     self.geolocate(self.start), 
                     self.geolocate(self.end),
                     steps)]
+
