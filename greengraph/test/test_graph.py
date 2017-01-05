a5cd4481562a3484834957d088d183768fe592a5
Gareth Jones <garethsion@googlemail.com>
2017-01-05 22:35:07 +0000

    Added test for green_between

--- greengraph/test/test_graph.py
+++ greengraph/test/test_graph.py
@@ -3,7 +3,8 @@ from greengraph import Greengraph
 from nose.tools import assert_equal
 import yaml
 import os
-import numpy.testing as npt
+import numpy.testing as npt 
+from mock import Mock, patch
 
 def test_geolocate():
     with open(os.path.join(os.path.dirname(__file__),
@@ -22,4 +23,16 @@ def test_location_sequence():
         for fixture in fixtures:
             my_graph = Greengraph('London', 'Cardiff')
             result = fixture.pop('result')
-            npt.assert_array_eual(my_graph.location_sequence(**fixture),result)
+            npt.assert_array_equal(my_graph.location_sequence(**fixture), result, 
+                    "Error in location_sequence")
+
+def test_green_between():
+    directory = os.path.join(os.path.dirname(__file__), 'fixtures', 'london_sat.png')
+    mock_start = open(directory, 'rb')
+    mock_end = open(directory, 'rb')
+    london_greenness = 108032
+
+    with patch('requests.get', return_value=Mock(content=mock_start.read())) as mock_get:
+        result = Greengraph('Cardiff', 'London')
+        mock_green = result.green_between(2)
+        npt.assert_array_almost_equal(mock_green, [london_greenness, london_greenness], decimal=2)
