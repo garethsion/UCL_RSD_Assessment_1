52fe7df1619bded4edd91a3bc78dedbaefe0e404
Gareth Jones <garethsion@googlemail.com>
2017-01-05 22:12:55 +0000

    Added test for location_sequence

--- greengraph/test/test_graph.py
+++ greengraph/test/test_graph.py
@@ -10,7 +10,16 @@ def test_geolocate():
                            'fixtures', 'journey.yaml')) as fixtures_file:
         fixtures = yaml.load(fixtures_file)
         for fixture in fixtures:
-            answer = Greengraph(**fixture)
-            assert_equal(answer.start, fixture['start'])
-            assert_equal(answer.end, fixture['end'])
-            assert_equal(answer.geocoder.domain, "maps.google.co.uk")
+            result = Greengraph(**fixture)
+            assert_equal(result.start, fixture['start'])
+            assert_equal(result.end, fixture['end'])
+            assert_equal(result.geocoder.domain, "maps.google.co.uk")
+
+def test_location_sequence():
+    with open(os.path.join(os.path.dirname(__file__),
+                            'fixtures', 'location_sequence.yaml')) as fixtures_file:
+        fixtures = yaml.load(fixtures_file)
+        for fixture in fixtures:
+            my_graph = Greengraph('London', 'Cardiff')
+            result = fixture.pop('result')
+            npt.assert_array_eual(my_graph.location_sequence(**fixture),result)
