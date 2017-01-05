95f79f6310fa83ed77d7dad6faafdcfc2ab615c5
Gareth Jones <garethsion@googlemail.com>
2017-01-05 23:49:25 +0000

    Removed reduntant code

--- greengraph/graph.py
+++ greengraph/graph.py
@@ -13,20 +13,6 @@ class Greengraph(object):
         return self.geocoder.geocode(place, exactly_one=False)[0][1]
     
     def location_sequence(self, start,end,steps):
-        # Raise an error if the input argument types do not match what is expected
-        if type(start) != str:
-            raise TypeError("Source should be a *string* value")
-        if type(end) != str:
-            raise TypeError("Destination should be a *string* value")
-        
-        # Raise an error if steps is not a number 
-        if type(steps) != int & type(steps) != float:
-            raise TypeError("Step size must be an *integer* or *floating point* value") 
-        
-        # Raise an error if steps is negative
-        if steps < 0:
-            raise ValueError("Step size must be positive")
-
         lats = np.linspace(start[0], end[0], steps)
         longs = np.linspace(start[1],end[1], steps)
         return np.vstack([lats, longs]).transpose()
