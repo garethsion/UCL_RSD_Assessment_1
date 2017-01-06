4bc829c2e5cc52ad8f9a0466ffbb3bf5b679e420
Gareth Jones <garethsion@googlemail.com>
2017-01-06 00:15:51 +0000

    Allowed arguments to be optional

--- greengraph/command.py
+++ greengraph/command.py
@@ -7,13 +7,13 @@ def process():
     parser = ArgumentParser(description = "Generate a graph showing the ammount " +  
             "of green space between two geographical locations by determining" + 
             " the number of green pixels in a satellite image")
-    parser.add_argument('--start', '-s', default="London", required=True, 
+    parser.add_argument('--start', '-s', default="London", required=False, 
             help='Starting location')
-    parser.add_argument('--end', '-d', default="Cardiff", required=True, 
+    parser.add_argument('--end', '-d', default="Cardiff", required=False, 
             help='End Location')
-    parser.add_argument('--steps', '-p', default="50", required=True,
+    parser.add_argument('--steps', '-p', default="50", required=False,
             help='Number of steps between the start and end locations')
-    parser.add_argument('--out', '-o', default="graph.png", required=True, 
+    parser.add_argument('--out', '-o', default="graph.png", required=False, 
             help='Output file name')   
 
     arguments = parser.parse_args()    
